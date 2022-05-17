2136dfa02a8b48cda02548afdc427fa77b7e3390
yunjukim <yjkim@imagoworks.ai>
2022-05-17 14:51:09 +0900

    project commit

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './DTO/create-movie.dto';
+import { Movie } from './entities/Movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id); // +(string)==parseInt(string) convert string into a number
+    if (!movie) {
+      throw new NotFoundException(`Movie with Id ${id} not found.`);
+    }
+    return movie;
+  }
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+  }
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: number, UpdateMovieDto) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...UpdateMovieDto });
+  }
+}
